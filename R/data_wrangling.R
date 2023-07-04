2736e362b3d3d2ddc652f1bb81d97b247150c708
Anil Tellbuescher <anil@tellbuescher.online>
2023-07-04 14:29:32 +0200

    add wrangling script; create gps coordinates for map plotting

---
+++ R/data_wrangling.R
@@ -0,0 +1,8 @@
+### MAP PLOT DATA-----------------------------------
+gps_coordinates <- tibble(gps = data$water$GPS,
+                              location = data$water$Location,
+                              city = data$water$City,
+                              country = data$water$Country) %>%
+  drop_na() %>%
+  separate(gps, into = c("latitude", "longitude"),
+           sep = ", ", convert = TRUE)
