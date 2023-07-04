74aac1491e1a058103aa07d2ee283e30a525d560
Anil Tellbuescher <anil@tellbuescher.online>
2023-07-04 15:12:45 +0200

    remove duplicated cities

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -4,5 +4,6 @@ gps_coordinates <- tibble(gps = data$water$GPS,
                               city = data$water$City,
                               country = data$water$Country) %>%
   drop_na() %>%
+  distinct(city, .keep_all = TRUE) %>%
   separate(gps, into = c("latitude", "longitude"),
            sep = ", ", convert = TRUE)
