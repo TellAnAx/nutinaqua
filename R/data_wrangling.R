6f44aa111f5f163009d4364d7e031ea0dc3a01ea
Anil Tellbuescher <anil@tellbuescher.online>
2023-07-04 18:51:05 +0200

    debug

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -3,7 +3,7 @@ gps_coordinates <- tibble(gps = data$water$GPS,
                               location = data$water$Location,
                               city = data$water$City,
                               country = data$water$Country) %>%
-  drop_na() %>%
+  drop_na(gps) %>%
   distinct(city, .keep_all = TRUE) %>%
   separate(gps, into = c("latitude", "longitude"),
            sep = ", ", convert = TRUE)
