9d2023942eae8bfa1ba3d4ed465da6d6f0938d5f
Anil Axel Tellbuscher <atellbuscher@frov.int>
2023-10-25 23:02:05 +0200

    create feed composition plot

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -1,9 +1,9 @@
 ### MAP PLOT DATA-----------------------------------
-gps_coordinates <- tibble(gps = data$water$GPS,
-                          location = data$water$Location,
-                          city = data$water$City,
-                          country = data$water$Country,
-                          source = data$water$Source) %>%
+gps_coordinates <- tibble(gps = data$water_tap$GPS,
+                          location = data$water_tap$Location,
+                          city = data$water_tap$City,
+                          country = data$water_tap$Country,
+                          source = data$water_tap$Source) %>%
   drop_na(gps) %>%
   distinct(city, .keep_all = TRUE) %>%
   separate(gps, into = c("latitude", "longitude"),
--- R/import_data.R
+++ R/import_data.R
@@ -1,5 +1,15 @@
 data <- list(
-  water = read_excel(here("data", "APO - Water quality.xlsx"),
-                     sheet = "municipalTap", skip = 1)
+  water_tap = import_from_server("Water%20quality%20APO-SUB-2.xlsx", 
+                                 "home/OneDrive-USB/APO-SUB-2/", 
+                                 sheet_name = "municipal_tap", 
+                                 skip = 1),
+  feed_commercial = import_from_server("Feed%20composition%20APO-SUB-2.xlsx", 
+                                       "home/OneDrive-USB/APO-SUB-2/", 
+                                       sheet_name = "data_commercial", 
+                                       skip = 1),
+  feed_experimental = import_from_server("Feed%20_composition%20APO-SUB-2.xlsx", 
+                                         "home/OneDrive-USB/APO-SUB-2/", 
+                                         sheet_name = "data_experimental", 
+                                         skip = 1)
 )
 
