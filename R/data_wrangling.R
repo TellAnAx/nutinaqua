a23c0a6e6cc9000e358aa35ea7ac7d17e26e2a69
Anil Tellbuescher <anil@tellbuescher.online>
2023-10-29 22:23:54 +0100

    update Google access token

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -1,9 +1,9 @@
 ### MAP PLOT DATA-----------------------------------
-gps_coordinates <- tibble(gps = data$water_tap$GPS,
-                          location = data$water_tap$Location,
-                          city = data$water_tap$City,
-                          country = data$water_tap$Country,
-                          source = data$water_tap$Source) %>%
+gps_coordinates <- tibble(gps = data$water_tap[[1]]$GPS,
+                          location = data$water_tap[[1]]$Location,
+                          city = data$water_tap[[1]]$City,
+                          country = data$water_tap[[1]]$Country,
+                          source = data$water_tap[[1]]$Source) %>%
   drop_na(gps) %>%
   distinct(city, .keep_all = TRUE) %>%
   separate(gps, into = c("latitude", "longitude"),
