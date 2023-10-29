e8e67aae8417d149a90e52a0e33bf5d61ad7d4e9
Anil Tellbuescher <anil@tellbuescher.online>
2023-10-29 22:24:54 +0100

    make sure data import does not fail if one file cannot be loaded

--- R/import_data.R
+++ R/import_data.R
@@ -1,15 +1,17 @@
-data <- list(
-  water_tap = import_from_server("Water%20quality%20APO-SUB-2.xlsx", 
-                                 "home/OneDrive-USB/APO-SUB-2/", 
-                                 sheet_name = "municipal_tap", 
-                                 skip = 1),
-  feed_commercial = import_from_server("Feed%20composition%20APO-SUB-2.xlsx", 
-                                       "home/OneDrive-USB/APO-SUB-2/", 
-                                       sheet_name = "data_commercial", 
-                                       skip = 1),
-  feed_experimental = import_from_server("Feed%20_composition%20APO-SUB-2.xlsx", 
-                                         "home/OneDrive-USB/APO-SUB-2/", 
-                                         sheet_name = "data_experimental", 
-                                         skip = 1)
-)
+data <- list()
+
+data$water_tap <- import_from_server("Water%20quality%20APO-SUB-2.xlsx", 
+                                     "home/OneDrive-USB/APO-SUB-2/", 
+                                     sheet_name = "municipal_tap", 
+                                     skip = 1)
+
+data$feed_commercial <- import_from_server("Feed%20composition%20APO-SUB-2.xlsx", 
+                                           "home/OneDrive-USB/APO-SUB-2/", 
+                                           sheet_name = "data_commercial", 
+                                           skip = 1)
+
+data$feed_experimental <- import_from_server("Feed%20composition%20APO-SUB-2.xlsx", 
+                                             "home/OneDrive-USB/APO-SUB-2/", 
+                                             sheet_name = "data_experimental", 
+                                             skip = 1)
 
