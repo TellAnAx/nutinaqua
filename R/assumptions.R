57ec908cab08796717dbb8e9a443b7ecf9e79f2d
Anil Tellbuescher <anil@tellbuescher.online>
2023-11-17 00:52:00 +0100

    update manuscript

--- R/assumptions.R
+++ R/assumptions.R
@@ -1,7 +1,5 @@
-assumptions <- list(
-  ER = 3.83,    # Daily water exchange rate (%/V/day) Kloas et al., 2015  
-  SD = 48.8,  # Fish density (kg/m^3)
-  FR = 1.25,    # Daily feeding rate per biomass unit (%/kg/day)
-  rearingV = 7.91,   # Volume of the rearing compartment (m^3) Kloas et al., 2015
-  totalV = 13   # Total volume of the aquaculture unit (m^3) Kloas et al., 2015
-)
+ER = 3.83    # Daily water exchange rate (%/V/day) Kloas et al., 2015  
+SD = 48.8  # Fish density (kg/m^3)
+FR = 1.25    # Daily feeding rate per biomass unit (%/kg/day)
+rearingV = 7.91   # Volume of the rearing compartment (m^3) Kloas et al., 2015
+totalV = 13   # Total volume of the aquaculture unit (m^3) Kloas et al., 2015
