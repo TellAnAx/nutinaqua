79086cb3883afe75599a6ee190735be2142c9dd5
Anil Tellbuescher <anil@tellbuescher.online>
2023-12-04 20:14:59 +0100

    add IAFFD data for import

--- R/import_data.R
+++ R/import_data.R
@@ -10,3 +10,11 @@ data$feed <- import_from_server("Feed%20composition%20APO-SUB-2.xlsx",
                                            sheet_name = "data", 
                                            skip = 1)
 
+data$iaffd_overview <- import_from_server("IAFFD%20FICD.xlsx",
+                                "home/OneDrive-USB/Data/", 
+                                sheet_name = "overview")
+
+data$iaffd_composition <- import_from_server("IAFFD%20FICD.xlsx",
+                                          "home/OneDrive-USB/Data/", 
+                                          sheet_name = "composition")
+
